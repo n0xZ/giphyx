45b42a7d3efba099d2cce0d8329651b5ff977287
n0xZ <o.gonzalo@hotmail.com>
2022-07-01 19:52:37 -0300

    set vercel URL and new media domain on next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,16 @@ module.exports = {
 	env: {
 		GIPHY_API_KEY: process.env.GIPHY_API_KEY,
 		GIPHY_API_URL: process.env.GIPHY_API_URL,
+		VERCEL_URL: process.env.VERCEL_URL,
 	},
 	images: {
 		domains: [
-			'media3.giphy.com',
 			'media0.giphy.com',
-			'media2.giphy.com',
 			'media1.giphy.com',
+			'media2.giphy.com',
+			'media3.giphy.com',
+			'media4.giphy.com',
+			'media5.giphy.com',
 		],
 	},
 }
