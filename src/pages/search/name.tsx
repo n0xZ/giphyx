de85de65f3287f8d7706d90b7af692838d326b5a
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-01 16:02:36 -0300

    removed api services and set its functionality to trpc files

--- src/pages/search/name.tsx
+++ src/pages/search/name.tsx
@@ -1,10 +1,8 @@
 import { useState, ChangeEvent, FormEvent } from 'react'
-import { useQuery } from 'react-query'
 import { FetchError } from '~/components/error/FetchError'
 import { GifList } from '~/components/gif/GifList'
 import { MainLayout } from '~/components/layout'
 import { Loading } from '~/components/loading/Loading'
-import { getSearchResultsByQuery } from '~/services/api'
 import { trpc } from '~/utils/trpc'
 import { SearchResultsI } from '~/types'
 
