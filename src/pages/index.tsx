36d9ed0b256e7d3f82c20cd53553a2d636ee4427
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-01 16:04:03 -0300

    set initial home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from 'next/link'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<main>
+			<p>Bienvenido a Giphyx!</p>
+			<p>Encuentre sus gifs favoritos de Giphy, y guardelos!</p>
+			<Link href="/search">Comenzar ya</Link>
+		</main>
+	)
 }
 
 export default Home
