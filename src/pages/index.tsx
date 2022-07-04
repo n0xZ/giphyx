394413ab85964a033b5341b7e113fb04f33b310d
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-04 16:23:26 -0300

    add initial content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,25 @@ import Link from 'next/link'
 
 const Home = () => {
 	return (
-		<main>
+		<main className="container grid min-h-screen grid-cols-2 mx-auto place-items-center">
 			<Head>
 				<title>Bienvenido a Giphyx!</title>
 			</Head>
-			<p>Bienvenido a Giphyx!</p>
-			<p>Encuentre sus gifs favoritos de Giphy, y guardelos!</p>
-			<Link href="/search">Comenzar ya</Link>
+			<section className="flex flex-col items-center space-y-5">
+				<h1 className="text-5xl tracking-wide text-center ">
+					Busque sus{' '}
+					<span className="text-transparent font-bold bg-clip-text bg-gradient-to-r from-[#7928ca] to-[#ff0080]">
+						gifs favoritos de Giphy
+					</span>{' '}
+					y guardelos.
+				</h1>
+				<Link href="/search">
+					<button className="p-[2px] bg-gradient-to-r from-[#7928ca] to-[#ff0080]  rounded-xl">
+						<aside className="p-10 bg-black hover:bg-slate-600 rounded-xl">Comenzar ya</aside>
+					</button>
+				</Link>
+			</section>
+			<section></section>
 		</main>
 	)
 }
