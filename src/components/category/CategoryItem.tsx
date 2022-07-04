9a23401e4055fa9a7b8a69608899e640bbc590fb
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-04 15:30:46 -0300

    set category image placeholder with its fallback SRC

--- src/components/category/CategoryItem.tsx
+++ src/components/category/CategoryItem.tsx
@@ -1,26 +1,30 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
+import Image from 'next/future/image'
 import { Data } from '~/types'
 interface Props {
 	category: Data
 }
 const CategoryItem: FC<Props> = ({ category }) => {
+	const [src, setSrc] = useState(category.gif.images['480w_still'].url)
+	const handleFallbackSRC = () => {
+		setSrc(category.gif.images.original.url)
+	}
 	return (
-		<Link
-			href={`/results/${category.name_encoded}`}
-			className="w-full duration-150 ease-in-out shadow-xl  rounded-xl hover:opacity-90"
-		>
-			<aside className="grid place-items-center ">
+		<Link href={`/results/${category.name_encoded}`}>
+			<article className="flex flex-col justify-center w-full shadow-xl rounded-xl hover:opacity-80">
 				<h3 className="text-2xl font-bold text-center">{category.name}</h3>
 				<Image
-					src={category.gif.images['480w_still'].url}
-					className="object-cover"
+					src={src}
+					className="rounded-xl w-full bg-gradient-to-r p-[3px] from-[#3a6186] to-[#89253e]"
+					placeholder="blur"
+					blurDataURL={category.gif.images['480w_still'].url}
+					onError={() => handleFallbackSRC()}
 					width={category.gif.images['480w_still'].width}
 					height={category.gif.images['480w_still'].height}
 					alt={category.gif.slug}
 				/>
-			</aside>
+			</article>
 		</Link>
 	)
 }
