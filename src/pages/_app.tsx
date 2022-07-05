f67a6c57f0f870e9aef13fe69dd073c7ae5d12c4
n0xZ <o.gonzalo@hotmail.com>
2022-07-05 07:40:43 -0300

    set react hot toast on app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import { withTRPC } from '@trpc/next'
 import { AnimatePresence } from 'framer-motion'
 import superjson from 'superjson'
+import { Toaster } from 'react-hot-toast'
 import { AppType } from 'next/dist/shared/lib/utils'
 import type { AppRouter } from '~/backend/routers/_app'
 import '../styles/app.css'
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<AnimatePresence exitBeforeEnter>
-			<Component {...pageProps} />
-		</AnimatePresence>
+		<>
+			<AnimatePresence exitBeforeEnter>
+				<Component {...pageProps} />
+			</AnimatePresence>
+			<Toaster position="top-right" />
+		</>
 	)
 }
 
@@ -21,13 +25,15 @@ export default withTRPC<AppRouter>({
 		const url = process.env.VERCEL_URL
 			? `https://${process.env.VERCEL_URL}/api/trpc`
 			: 'http://localhost:3000/api/trpc'
-
 		return {
 			url,
+
 			/**
 			 * @link https://react-query.tanstack.com/reference/QueryClient
 			 */
-			queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+			queryClientConfig: {
+				defaultOptions: { queries: { staleTime: 60, useErrorBoundary: true } },
+			},
 			headers() {
 				if (ctx?.req) {
 					return {
@@ -37,7 +43,7 @@ export default withTRPC<AppRouter>({
 				}
 				return {}
 			},
-			transformer:superjson
+			transformer: superjson,
 		}
 	},
 	/**
