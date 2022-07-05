d7b3a9ef3ed187a6ada2dfb7e1d31a823b74dc95
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-05 08:33:07 -0300

    set redirect if user's already logged in

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,12 +1,25 @@
 import { NextPage } from 'next'
 import { signIn } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
 const SignIn: NextPage = () => {
+	const { data: session } = useSession()
+	const router = useRouter()
+	if (session) router.push('/search')
 	return (
-		<main className="grid min-h-screen place-items-center">
+		<motion.main
+			key={router.pathname}
+			exit={{ opacity: 0 }}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			className="grid min-h-screen place-items-center"
+		>
+			<h2>Para poder acceder a Giphyx, se debe iniciar sesión.</h2>
 			<button onClick={() => signIn('github')}>
 				Iniciar sesión desde Github
 			</button>
-		</main>
+		</motion.main>
 	)
 }
 
