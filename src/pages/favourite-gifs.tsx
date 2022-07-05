22918bab27345afe58104780f9d4f9940d2be975
n0xZ <o.gonzalo@hotmail.com>
2022-07-05 19:22:59 -0300

    set EmptyFavouriteGifs comp with assets related to search

--- src/pages/favourite-gifs.tsx
+++ src/pages/favourite-gifs.tsx
@@ -1,19 +1,58 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/future/image'
 import { useSession } from 'next-auth/react'
 import { GifList } from '~/components/gif/GifList'
 import { MainLayout } from '~/components/layout'
 import ProtectedContent from '~/components/protected/ProtectedContent'
+import CategoriesSearchAsset from '~/public/search-category.svg'
+import NameSearchAsset from '~/public/search-name.svg'
 import { GIF } from '~/types'
 
 const EmptyFavouriteGIFS = () => {
 	return (
-		<section>
-			Al parecer no tienes GIFS Favoritos. Puedes elegirlos aquí:
-			<Link href="/search/category">Por categoría</Link>
-			<Link href="/search/category">Por nombre</Link>
-		</section>
+		<MainLayout>
+			<h2 className="mt-6 text-lg font-bold text-center xl:text-2xl">
+				Al parecer no tienes GIFS Favoritos. Puedes elegirlos aquí:
+			</h2>
+			<section className="container flex flex-col items-center justify-center h-screen grid-cols-2 mx-auto space-y-8 xl:space-x-5 xl:space-y-0 xl:flex-row xl:justify-center ">
+				<article className="grid gap-3 p-2 duration-100 ease-in border-2 border-black place-items-center hover:-translate-y-1 rounded-xl hover:border-purple-800">
+					<Image
+						className="w-3/4 "
+						src={CategoriesSearchAsset.src}
+						width={CategoriesSearchAsset.width}
+						priority={true}
+						height={CategoriesSearchAsset.height}
+					/>
+
+					<button className="bg-gradient-to-r p-[1px] from-[#7928ca] to-[#ff0080] rounded-xl hover:opacity-60">
+						<Link href="/search/category">
+							<aside className="p-5 font-bold dark:bg-black rounded-xl">
+								Buscar por categoría
+							</aside>
+						</Link>
+					</button>
+				</article>
+
+				<article className="grid gap-3 p-3 duration-100 ease-in border-2 border-black place-items-center hover:-translate-y-1 rounded-xl hover:border-[#7928ca]">
+					<Image
+						className="w-3/4 dark:bg-black rounded-xl"
+						src={NameSearchAsset.src}
+						width={NameSearchAsset.width}
+						priority={true}
+						height={NameSearchAsset.height}
+					/>
+					<button className="bg-gradient-to-r p-[1px] from-[#7928ca] to-[#ff0080] rounded-xl hover:opacity-60">
+						<Link href="/search/name">
+							<aside className="p-5 font-bold dark:bg-black rounded-xl">
+								Buscar por nombre
+							</aside>
+						</Link>
+					</button>
+				</article>
+			</section>
+		</MainLayout>
 	)
 }
 
@@ -22,21 +61,19 @@ const FavouriteGifs: NextPage = () => {
 	if (!session) return <ProtectedContent />
 
 	if (localStorage.getItem('favourite-gifs') === null)
-		return (
-			<MainLayout>
-				<EmptyFavouriteGIFS />
-			</MainLayout>
-		)
+		return <EmptyFavouriteGIFS />
 	const selectedGIFS = JSON.parse(
 		String(localStorage.getItem('favourite-gifs'))
 	) as GIF[]
-	
+
 	return (
 		<MainLayout>
 			<Head>
 				<title>Giphyx - Mis GIFS Favoritos</title>
 			</Head>
-			<h2 className="text-center">Mis GIFS Favoritos</h2>
+			<h2 className="mt-6 text-lg font-bold text-center xl:text-2xl">
+				Mis GIFS Favoritos
+			</h2>
 			<GifList gifs={selectedGIFS} />
 		</MainLayout>
 	)
