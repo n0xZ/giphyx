3f46b4a702ce8772b566460a01667468203c2329
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-05 08:11:08 -0300

    set github oauth credentials to next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ module.exports = {
 		GIPHY_API_KEY: process.env.GIPHY_API_KEY,
 		GIPHY_API_URL: process.env.GIPHY_API_URL,
 		VERCEL_URL: process.env.VERCEL_URL,
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+		GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 	},
 	images: {
 		domains: [
