3b6cf335e6de63288a016a676265036589e047b8
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-05 11:01:46 -0300

    add assets and set 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/future/image'
+import Link from 'next/link'
+import { MainLayout } from '~/components/layout'
+import Asset404 from '~/public/404.svg'
+const Page404 = () => {
+	return (
+		<MainLayout>
+			<section className="flex flex-col items-center justify-center min-h-screen space-y-3">
+				<Image
+					className="rounded-xl bg-gradient-to-r p-[2px] bg-black"
+          width={500}
+          height={500}
+					src={Asset404.src}
+					priority={true}
+				/>
+				<h1 className="text-xl font-bold">
+					Ups! Al parecer esta página no existe
+				</h1>
+				<Link href="/search">Volver atrás</Link>
+			</section>
+		</MainLayout>
+	)
+}
+
+export default Page404
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from 'next'
+import Image from 'next/future/image'
 import { signIn } from 'next-auth/react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { motion } from 'framer-motion'
+import { Icon } from '@iconify/react'
+import SignInAsset from '~/public/sign-in-asset.svg'
 const SignIn: NextPage = () => {
-	const { data: session } = useSession()
+	const { data: session, status } = useSession()
 	const router = useRouter()
 	if (session) router.push('/search')
 	return (
@@ -13,11 +16,25 @@ const SignIn: NextPage = () => {
 			exit={{ opacity: 0 }}
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
-			className="grid min-h-screen place-items-center"
+			className="flex flex-col items-center justify-center min-h-screen mt-12 space-y-6"
 		>
-			<h2>Para poder acceder a Giphyx, se debe iniciar sesión.</h2>
-			<button onClick={() => signIn('github')}>
-				Iniciar sesión desde Github
+			<Image
+				className="w-3/4 rounded-xl bg-gradient-to-r p-[2px] from-[#7928ca] to-[#ff0080] "
+				src={SignInAsset.src}
+				width={SignInAsset.width}
+				priority={true}
+				height={SignInAsset.height}
+			/>
+			<h2 className="text-xl font-bold text-center">
+				Para poder acceder a Giphyx, se debe iniciar sesión.
+			</h2>
+			<button
+				disabled={status === 'loading'}
+				className="flex flex-row items-center px-5 py-5 mb-6 space-x-3 font-bold text-center ease-in border-2 rounded-lg border-zinc-800 hover:opacity-80"
+				onClick={() => signIn('github')}
+			>
+				<Icon icon="akar-icons:github-fill" />
+				<span>Iniciar sesión desde Github</span>
 			</button>
 		</motion.main>
 	)
