3cbf433ec9e8798404d760c2450a7e9057f2885a
n0xZ <o.gonzalo@hotmail.com>
2022-07-04 22:44:22 -0300

    set initial search page layout

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -8,11 +8,27 @@ const SearchPage: NextPage = () => {
 		<MainLayout>
 			<Head>
 				<title>Giphyx - Buscar gifs</title>
-			</Head>
-			<h2>Cómo desea realizar la busqueda de GIFS?</h2>
+			</Head>{' '}
+			<h2 className="mt-6 text-center">
+				Cómo desea realizar la busqueda de GIFS?
+			</h2>
+			<section className="container grid h-screen grid-cols-2 mx-auto place-items-center">
+				<button className="bg-gradient-to-r p-[1px] from-[#7928ca] to-[#ff0080] rounded-xl hover:opacity-60">
+					<Link href="/search/category">
+						<aside className="p-5 dark:bg-zinc-900 rounded-xl">
+							Buscar por categoría
+						</aside>
+					</Link>
+				</button>
 
-			<Link href="/search/category">Buscar por categoría</Link>
-			<Link href="/search/name">Buscar por nombre</Link>
+				<button className="bg-gradient-to-r p-[1px] from-[#7928ca] to-[#ff0080] rounded-xl hover:opacity-60">
+					<Link href="/search/name">
+						<aside className="p-5 dark:bg-zinc-900 rounded-xl">
+							Buscar por nombre
+						</aside>
+					</Link>
+				</button>
+			</section>
 		</MainLayout>
 	)
 }
