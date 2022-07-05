08dbc2e79287268ecfb0c0cee4b30ba51f70d1fd
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-07-05 09:41:58 -0300

    add protectedContent component to display when user's trying to access to private routes

---
+++ src/components/protected/ProtectedContent.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { MainLayout } from '../layout'
+
+const ProtectedContent = () => {
+	const [timer, setTimer] = useState(3)
+	const router = useRouter()
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setTimer((prev) => prev - 1)
+		}, 1000)
+		return () => clearTimeout(timer)
+	})
+	if (timer === 0) router.push('sign-in')
+	return (
+		<MainLayout>
+			<section className="mt-2">
+				<p>
+					Tienes que iniciar sesión para poder ver el contenido. Serás
+					reedirigido en {timer}
+				</p>
+			</section>
+		</MainLayout>
+	)
+}
+
+export default ProtectedContent
